2b45604ec498717840d08e2d4e0d269ac5ece7bc
Luka <looka1998@gmail.com>
2019-03-23 21:08:20 +0100

    Temporary output of login result
    - This is temporary only! Remove after frontend update.

--- src/Controller/Homepage/DefaultController.php
+++ src/Controller/Homepage/DefaultController.php
@@ -4,11 +4,19 @@ namespace App\Controller\Homepage;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class DefaultController extends AbstractController
 {
-    public function index(TokenStorageInterface $tokenStorage)
+    public function index(TokenStorageInterface $tokenStorage, AuthenticationUtils $authUtils)
     {
+        if ($authUtils->getLastAuthenticationError() != null)
+            $errorMsg = "Invalid login";
+        else
+            $errorMsg = "No error";
+
+        echo $errorMsg;
+
         return $this->render("homepage/homepage.html.twig", [
             "page_title" => "Bekdur aplikacija",
             "user_name" => $tokenStorage->getToken()->getUsername(),
