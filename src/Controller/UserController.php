1097e04ab5270f54f544d3f4f35658969ec7c4f0
Luka <looka1998@gmail.com>
2019-03-23 12:11:09 +0100

    Added a function for sample user creation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class UserController extends AbstractController
@@ -11,5 +14,20 @@ class UserController extends AbstractController
     public function new()
     {
         return new Response("OK!");
+    }
+
+    public function new_sample(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        
+        $user->setUsername("test_user_".rand(1, 100000));
+        $user->setFirstName("Userko");
+        $user->setLastName("Useric");
+        $user->setPassword($encoder->encodePassword($user, "blank"));
+
+        $em->persist($user);
+        $em->flush();
+
+        return new Response("Added user: ".$user->getUsername());
     }
 }
