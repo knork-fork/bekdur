0454ae1f8cd6ea7c5418073cef0494a86b5fd490
Luka <looka1998@gmail.com>
2019-06-14 17:31:28 +0200

    Add mobile device detection

--- src/Controller/Homepage/DefaultController.php
+++ src/Controller/Homepage/DefaultController.php
@@ -4,11 +4,13 @@ namespace App\Controller\Homepage;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-
+use App\Service\MobileDetector;
 class DefaultController extends AbstractController
 {
-    public function index(TokenStorageInterface $tokenStorage)
+    public function index(TokenStorageInterface $tokenStorage, MobileDetector $mobileDetector)
     {
+        //dd($mobileDetector->isMobile());
+
         return $this->render("homepage/homepage.html.twig", [
             "page_title" => "Collabsy",
             "user_name" => $tokenStorage->getToken()->getUsername(),
